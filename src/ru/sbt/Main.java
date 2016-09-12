c2e2c61ef80db1e3d861321263d440cab99a2277
vitaly-emelianov <vitalik.emelianov@gmail.com>
2016-09-12 16:13:24 +0300

    FIx

--- src/ru/sbt/Main.java
+++ src/ru/sbt/Main.java
@@ -4,4 +4,12 @@ package ru.sbt;
  * Created by vitaly on 9/12/16.
  */
 public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        run(x);
+        System.out.println(x);
+    }
+    public static void run(int x) { // передается копия (ссылки, примитивного типа)
+        x= 12; //
+    }
 }
--- src/ru/sbt/Person.java
+++ src/ru/sbt/Person.java
@@ -4,6 +4,14 @@ package ru.sbt;
  * Created by vitaly on 9/12/16.
  */
 public class Person {
-    public Person() {
+    private final String name; // меняется ссылка только один раз
+    public int age; // можно менять в люмбом месте
+    protected int kids_number; // Видно в потомках
+
+
+    public Person(String name, int age, int kids_number) {
+        this.name = name;
+        this.age = age;
+        this.kids_number = kids_number;
     }
 }
