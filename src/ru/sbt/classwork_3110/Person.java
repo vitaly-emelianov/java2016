7da5152c5fde82a3850eed22b74c7666cc9884e0
vitaly-emelianov <vitalik.emelianov@gmail.com>
2016-10-31 16:37:35 +0300

    ReflectionUtils class was added.

---
+++ src/ru/sbt/classwork_3110/Person.java
@@ -0,0 +1,28 @@
+package ru.sbt.classwork_3110;
+
+/**
+ * Created by vitaly on 31.10.16.
+ */
+public class Person {
+    private final String name;
+    private final String surname;
+    private final int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/ru/sbt/classwork_3110/ReflectionUtils.java
@@ -0,0 +1,57 @@
+package ru.sbt.classwork_3110;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by vitaly on 31.10.16.
+ */
+public class ReflectionUtils {
+    public static List<Field> printMethodInfo (Object o) {
+        List<Field> fields = new ArrayList<>();
+        Class<?> clazz = o.getClass();
+        while (clazz != null) {
+            fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
+        return fields;
+    }
+
+    public static List<Object> printAllInvokedGetters(Object o) {
+        List<Object> invocations = new ArrayList<>();
+        Class<?> clazz  = o.getClass();
+        while (clazz != null) {
+            List<Method> getters = new ArrayList<>();
+            for (Method method: clazz.getDeclaredMethods()) {
+                if (method.getName().startsWith("get")) {
+                    getters.add(method);
+                    try {
+                        method.setAccessible(true);
+                        invocations.add(method.invoke(o));
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return invocations;
+    }
+
+
+    public static void main(String[] args) {
+        Integer a = 12;
+        ReflectionUtils reflectionUtil = new ReflectionUtils();
+        Person p = new Person("jack", "russel", 122);
+        System.out.println(reflectionUtil.printAllInvokedGetters(p));
+    }
+}
+
